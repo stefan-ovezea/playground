19c53964ae8bda0e5e2d6c97620c4808596b5667
stefanovezea <Stefan.Ovezea@ro.ibm.com>
2017-10-15 19:07:41 +0300

    Wrapped up binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -7,23 +7,35 @@ int main() {
 
     std::array<int, 11> arr = {11, 21, 34, 45, 53, 67, 78, 82, 96, 110, 123};
     int num = 78;
-    std::cout << "Array size: " << arr.size() << "\n";
     
-    binarySearch(arr, num);
+    int result = binarySearch(arr, num);
+    
+    if (result == -1) {
+        std::cout << "Number not found!\n";
+    } else {
+        std::cout << "Found the number " << num << " at position " << result << ".\n";
+    }
     return 0;
 }
 
 int binarySearch(auto &arr, int num) {
     int leftPos = 0;
     int rigthPos = arr.size() - 1;
-    int opperationsCounter = 0;
+    int operationsCounter = 1;
 
     while (leftPos <= rigthPos) {
         int mid = (leftPos + rigthPos) / 2;
         if (arr[mid] == num) {
-            return arr[mid];
+            std::cout << "Finished after " << operationsCounter << " operations.\n";
+            return mid;
+        } else if (arr[mid] <= num) {
+            leftPos = mid;
+        } else {
+            rigthPos = mid;
         }
+        operationsCounter++;
     }
-    
+
+    std::cout << "Finished after " << operationsCounter << " operations.\n";
     return -1;
 }
